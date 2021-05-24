acb1d8a2990ef59aff62a1089ebc28bc6b7a88d9
kotaewing <kota.ewing@gmail.com>
2021-05-24 10:43:10 -0600

    working on removing notes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { notes } = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
-const htmlRoutes = require('./routes/htmlRoutes')
 const uniqid = require('uniqid');
 
 const PORT = process.env.PORT || 3001;
@@ -17,6 +16,21 @@ function createNewNote(body, notesArray) {
         JSON.stringify({ notes: notesArray }, null, 2)
     );
     return note;
+}
+
+function findId(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+}
+
+function removeNote(notesArray, id) {
+    const newList = notesArray.filter(note => note.id !== id)
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: newList }, null, 2)
+    );
+    
+    return newList
 }
 
 app.use(express.static('public'));
@@ -37,6 +51,13 @@ app.post('/api/notes', (req, res) => {
 
     const note = createNewNote(req.body, notes);
     res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id
+    const note = removeNote(notes, noteId)
+    res.json(note);
+
 })
 
 app.get('/', (req, res) => {
