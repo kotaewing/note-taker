9e003e97e99c067bd72b4db6ed4165a309a43a84
kotaewing <kota.ewing@gmail.com>
2021-05-24 08:53:59 -0600

    html is now being served by server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { db } = require('db.json');
+const { db } = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,9 +12,21 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use(express.json());
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server is now on port ${PORT}`);
